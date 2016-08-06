cfae5cbcbdb2fabc1cdba9bbdf4d74efef380f2c
Miles Gullingsrud <miles.gullingsrud@gmail.com>
2016-08-06 13:53:40 -0700

    Renamed ITEMS to INVENTORY_ITEMS.

--- server/router.js
+++ server/router.js
@@ -1,13 +1,13 @@
 const router = require('express').Router();
 
-const ITEMS = [
+const INVENTORY_ITEMS = [
   { id: 1, name: 'Paper Towels', storageLocation: 'Kitchen Cupboard', targetQuantity: 12 },
   { id: 2, name: 'Napkins', storageLocation: 'Kitchen Cupboard', targetQuantity: 6 },
   { id: 3, name: 'Cholula', storageLocation: 'Kitchen Shelf', targetQuantity: 2 },
 ];
 
 router.get('/items', function(req, res) {
-  res.send(ITEMS);
+  res.send(INVENTORY_ITEMS);
 });
 
 router.post('/items', function(req, res) {
